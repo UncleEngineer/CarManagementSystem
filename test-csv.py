363e83ad7d2f4e152a2b755ad822af7876bc52e5
Uncle Engineer <loong.wissawakorn@gmail.com>
2022-05-07 15:33:53 +0700

    write to csv function

---
+++ test-csv.py
@@ -0,0 +1,12 @@
+import csv
+
+def writetocsv(data):
+	# data = ['toyota','red','1A11','1001','2022-05-07 15:29:15']
+	with open('2-car-system-in.csv','a',newline='',encoding='utf-8') as file:
+		fw = csv.writer(file)
+		fw.writerow(data) # no s is single line append
+	print('csv saved')
+	
+
+data = ['toyota','red','1A11','1001','2022-05-07 15:29:15']
+writetocsv(data)
